03e16889e0c6af27ff0421b84e772a5df33b2f4a
Goshiq <vangog-2005@ngs.ru>
2021-04-04 00:06:30 +0300

    wp is alive

--- srcs/mysql/init.sh
+++ srcs/mysql/init.sh
@@ -2,7 +2,11 @@
 
 apk update
 apk add telegraf --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted --no-cache
-apk add --no-cache mysql mysql-client
+apk add mysql mysql-client openrc
+
+openrc default
+/etc/init.d/mariadb setup
+rc-service mariadb start
 
 mkdir -p /etc/telegraf
 mv ./telegraf.conf /etc/telegraf
@@ -11,11 +15,13 @@ echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
 echo "CREATE USER 'user'@'%' IDENTIFIED BY 'password';" | mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'user'@'%' WITH GRANT OPTION;" | mysql -u root --skip-password
 echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
-#echo "DROP DATABASE test" | mysql -u root --skip-password
+echo "DROP DATABASE test" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 
 #mysql wordpress -u root --password=  < mydb.sql
 sed -i 's/skip-networking/#skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
+
+rc-service mariadb stop
 
 telegraf &
 /usr/bin/mysql_install_db --user=mysql --datadir="/var/lib/mysql"
--- srcs/wordpress/init.sh
+++ srcs/wordpress/init.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
 apk update
-apk add  --no-cache wget nginx supervisor php7 php7-fpm php7-opcache php7-gd php7-mysqli php7-zlib php7-curl php7-mbstring php7-json php7-session
+apk add  --no-cache wget php7 php7-fpm php7-opcache php7-gd php7-mysqli php7-zlib php7-curl php7-mbstring php7-json php7-session
+apk add telegraf --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted --no-cache
 
 adduser -D -g 'www' www
 mkdir -p /www
-chown -R www:www /var/lib/nginx
 chown -R www:www /www
-
-mv ./nginx.conf /etc/nginx/
-mv ./supervisord.conf /etc/
 
 wget https://wordpress.org/latest.tar.gz
 tar -xf latest.tar.gz
@@ -17,9 +14,12 @@ rm -f latest.tar.gz
 chmod 777 -R wordpress
 mv wordpress /www/
 mv wp-config.php /www/wordpress/
-nginx -t
 
-sed -i 's/;daemonize = yes/daemonize = no/g' /etc/php7/php-fpm.conf
-sed -i 's/;    extension=mysql.so/extension=mysql.so/g'  /etc/php7/php.ini
+#sed -i 's/;daemonize = yes/daemonize = no/g' /etc/php7/php-fpm.conf
+#sed -i 's/;    extension=mysql.so/extension=mysql.so/g'  /etc/php7/php.ini
 
-/usr/bin/supervisord -c /etc/supervisord.conf
+mkdir -p /etc/telegraf
+mv ./telegraf.conf /etc/telegraf
+
+telegraf &
+php -S 0.0.0.0:5050 -t /www/
