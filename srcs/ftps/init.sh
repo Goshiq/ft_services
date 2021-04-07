5a70ec4cf4c75399965f8b9d138090f8cdbd768d
Goshiq <vangog-2005@ngs.ru>
2021-04-07 18:48:18 +0300

    ftps is working

--- srcs/ftps/init.sh
+++ srcs/ftps/init.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
 apk update
-apk add vsftpd openssl
+apk add vsftpd
 apk add telegraf --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted --no-cache
 
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
-		-keyout /etc/ssl/private/ftps.key \
-		-out /etc/ssl/certs/ftps.crt \
-		-subj "/C=RU/ST=Kazan/L=Kazan/O=Jmogo/OU=Jmogo/CN=ftps"
-
-echo "root:root" | chpasswd
+mv ./hello /var/lib/ftp/
 
 mkdir -p /etc/telegraf
 mv ./telegraf.conf /etc/telegraf/
 
+mkdir -p /var/lib/ftp/upload/
+chmod 777 /var/lib/ftp/upload/
 mv ./vsftpd.conf /etc/vsftpd/
-touch /var/log/xferlog
 
 telegraf &
 vsftpd /etc/vsftpd/vsftpd.conf
